fd7f930a83f89583e0525027d0fec5319ea172ea
Denys Melnyk <georgiemuler@gmail.com>
2023-11-18 13:09:57 +0200

    initial commit

---
+++ InstagramReel/InstagramReel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstagramReel
+//
+//  Created by Georgie Muler on 18.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstagramReel/InstagramReel/InstagramReelApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramReelApp.swift
+//  InstagramReel
+//
+//  Created by Georgie Muler on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramReelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
